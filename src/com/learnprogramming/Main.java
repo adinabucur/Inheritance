4f1c07ca11cc8fadb6c83373fdc725d90858f639
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-08-29 12:48:41 +0300

    Update

--- src/com/learnprogramming/Main.java
+++ src/com/learnprogramming/Main.java
@@ -3,11 +3,17 @@ package com.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        Animal animal = new Animal("Animal", 1, 1, 5, 5);
-
-        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+//        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+//        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
 //        dog.eat();
-        dog.walk();
+//        dog.walk();
 //        dog.run();
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
     }
 }
